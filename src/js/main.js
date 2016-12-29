aac0125008c6608569de69c1c7a0142155fa6b2f
DaQunda Magwood <dmagwood13@gmail.com>
2016-12-29 15:54:34 -0500

    adjust some font

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ import angular from 'angular';
 import { routerConfig } from './route'
 
 import { LayoutController } from './controller/layout';
+import { ContactController } from './controller/contact';
+import { FolioDetailsController } from './controller/folioDetails';
+import { HomeController } from './controller/home';
+import { PortfolioController } from './controller/portfolio';
+import { ResumeController } from './controller/resume';
+
 
 import 'angular-ui-router';
 
@@ -10,3 +16,8 @@ angular
   .module('app', ['ui.router'])
   .config(routerConfig)
   .controller('LayoutController', LayoutController)
+  .controller('ContactController', ContactController)
+  .controller('FolioDetailsController', FolioDetailsController)
+  .controller('HomeController', HomeController)
+  .controller('PortfolioController', PortfolioController)
+  .controller('ResumeController', ResumeController)
