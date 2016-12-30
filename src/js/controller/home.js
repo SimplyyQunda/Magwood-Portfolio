2e8b2012edcb215f2b397a4a4de98bf0111e85d4
DaQunda Magwood <dmagwood13@gmail.com>
2016-12-30 18:34:48 -0500

    moving functionality from portfolio to home and some more functionality

--- src/js/controller/home.js
+++ src/js/controller/home.js
@@ -1,11 +1,32 @@
-function HomeController (allPortfolio) {
+import $ from 'jquery';
+
+function HomeController ($state, $stateParams, allPortfolio) {
 
 let vm = this;
 
 vm.all = allPortfolio;
+vm.scrollToTop = scrollToTop;
+vm.id = $stateParams.id;
 
-console.log (vm.all)	
+// console.log (vm.all)	
+
+
+ 	function scrollTo (className) {
+ 		$('body').animate({
+	  		scrollTop: $(className).offset().top
+	  	}, 900);
+	  	console.log('clicked')
+
+	  }
+
+	function scrollToTop (className) {
+	  	$('body').animate({
+	  		scrollTop: $(className).offset().top
+	  	}, 900);
+	  	console.log('clicked')
+	  }
+
 }
 
-HomeController.$inject = ['allPortfolio']  
+HomeController.$inject = ['$state', '$stateParams', 'allPortfolio']  
 export { HomeController };
---
+++ src/js/controller/portfolio.js
@@ -0,0 +1,22 @@
+// function PortfolioController ($state, $stateParams) {
+
+// 	let vm = this;
+
+	
+
+// 	 // $scope.scrollToTop = function (className) {
+//   //         $('body').animate({
+//   //           scrollTop: $(className).offset().top
+//   //         }, 900);
+//   //       };
+
+//   function scrollToTop (className) {
+//   	$('body').animate({
+//   		scrollTop: $(className).offset().Top
+//   	}, 900);
+
+//   }
+// }
+
+// PortfolioController.$inject = ['$state', '$stateParams'];
+// export { PortfolioController };
