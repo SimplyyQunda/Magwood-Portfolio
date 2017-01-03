55aaded4252aa51430d360903f4131312df5f705
DaQunda Magwood <dmagwood13@gmail.com>
2017-01-03 18:21:37 -0500

    portfolio array is created and done

--- src/js/folioarray.js
+++ src/js/folioarray.js
@@ -1,5 +1,57 @@
 const allPortfolio = [
 
+	{
+	  id: 0,
+      projName: 'PinPoint',
+      projTask: 'Help Youtube Users and Creators organize channels with custom categories',
+      projDescription: 'Typical everyone goes on YouTube at less once or consistently. As we all know a typical search on youtube is you have topic and you search it; then Youtube gives back thousands of results. PinPoint consist of custom categories where you can add your favorite channel rated by PinPoints users. Speaking of user, your allow to create your on profile and you can subscribe to your favorite category.',
+      FrontEndTool: 'AngularJS, JQuery',
+      backendTool: 'AdonisJS',
+      cssTool: 'SASS, Bulma',
+      projUrl: 'http://tiy-daqundamagwood-pinpoint-frontend.surge.sh',
+      githubUrl: 'https://github.com/SimplyyQunda/PinPoint',
+      thumbnail: 'images/pinpoint.png'	
+    },
+
+    {
+      id: 1,
+      projName: 'SnapOgram',
+      projTask: 'Instagram clone for one user with a natural & animal theme',
+      projDescription: 'SnapOgram is a Instagram clone that was created in a weekend with a partner. This site was created for our first Hackathron. After learning AngularJS, SnapOgram was the site were we put those powers to use along with using flexbox.',
+      FrontEndTool: 'AngularJS, JQuery',
+      backendTool: 'AdonisJS',
+      cssTool: 'SASS, Bulma',
+      projUrl: 'http://tiy-daqundamagwood-snapogram.surge.sh', 
+      githubUrl: 'https://github.com/SimplyyQunda/snapogram',
+      thumbnail: 'images/snapogram.png'
+    },
+
+    {
+      id: 2,
+      projName: 'KJI Public House',
+      projTask: 'A korean pub mockup resturant site',
+      projDescription: 'Here we have a korean resturant, that was created by three people. It took us a weekend to create this site; where we used our common friend AJAX request. We learned how to access the google maps. There were a great deal of merge conflicts and now I know how to handle those. Also in this project we were able to play with flexbox',
+      FrontEndTool: 'Javascript, JQuery, Lodash',
+      backendTool: 'None Needed',
+      cssTool: 'SASS',
+      projUrl: 'http://tiy-snehelpatel-team4restaurantpage.surge.sh',
+      githubUrl: 'https://github.com/team4restaurantpage/team4restaurantpage',
+      thumbnail: 'images/kjipublichouse.png',
+    },
+
+    {
+      id: 3,
+      projName: 'Peluza Dartie Enterprise',
+      projTask: 'Have you heard of Lawn Darts, If not, heres a mockup site about them !',
+      projDescription:'Peluza Dartie was also a weekend project, that has a theme of the lawn darts service. This site consist of using the basics of javascript. This site was created for practice for higher order to functions',
+      FrontEndTool: 'JQuery, Lodash',
+      backendTool: 'None Needed',
+      cssTool: 'SASS',
+      projUrl: 'http://tiy-daqundamagwood-lawndarts.surge.sh',
+      githubUrl: 'https://github.com/SimplyyQunda/Lollie-Dartie-Co.',
+      thumbnail: 'images/peluzadartie.png'
+    }
+
 ]
 
 export { allPortfolio }
